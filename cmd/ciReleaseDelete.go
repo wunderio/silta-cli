ed66ab3202a58f51287be5d7fd71f8e273854dff
Jānis Bebrītis <janis.bebritis@wunder.io>
2024-11-22 11:38:19 +0200

    Removal and cleanup deduplication

--- cmd/ciReleaseDelete.go
+++ cmd/ciReleaseDelete.go
@@ -1,13 +1,12 @@
 package cmd
 
 import (
-	"context"
 	"log"
 	"os"
 
 	helmclient "github.com/mittwald/go-helm-client"
 	"github.com/spf13/cobra"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/wunderio/silta-cli/internal/common"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // gcp auth provider
 	"k8s.io/client-go/tools/clientcmd"
@@ -60,61 +59,9 @@ var ciReleaseDeleteCmd = &cobra.Command{
 			log.Fatalf("Cannot create client from kubeConfig")
 		}
 
-		//Uninstall Helm release
-		uninstallErr := helmClient.UninstallReleaseByName(releaseName)
-		if uninstallErr != nil {
-			log.Fatalf("Error removing a release:%s", uninstallErr)
-		}
-
-		//Delete pre-release jobs
-		selectorLabels := []string{
-			"release",
-			"app.kubernetes.io/instance",
-		}
-
-		for _, l := range selectorLabels {
-			selector := l + "=" + releaseName
-			list, err := clientset.BatchV1().Jobs(namespace).List(context.TODO(), v1.ListOptions{
-				LabelSelector: selector,
-			})
-			if err != nil {
-				log.Fatalf("Error getting the list of jobs: %s", err)
-			}
-			for _, v := range list.Items {
-				log.Printf("Deleting job: %s", v.Name)
-				propagationPolicy := v1.DeletePropagationBackground
-				clientset.BatchV1().Jobs(namespace).Delete(context.TODO(), v.Name, v1.DeleteOptions{PropagationPolicy: &propagationPolicy})
-			}
-		}
-
-		//Delete PVCs
-		if deletePVCs {
-
-			PVC_client := clientset.CoreV1().PersistentVolumeClaims(namespace)
-
-			selectorLabels := []string{
-				"app",
-				"release",
-				"app.kubernetes.io/instance",
-			}
-
-			for _, l := range selectorLabels {
-				selector := l + "=" + releaseName
-				if l == "app" {
-					selector = l + "=" + releaseName + "-es"
-				}
-				list, err := PVC_client.List(context.TODO(), v1.ListOptions{
-					LabelSelector: selector,
-				})
-				if err != nil {
-					log.Fatalf("Error getting the list of PVCs: %s", err)
-				}
-
-				for _, v := range list.Items {
-					log.Printf("Deleting PVC: %s", v.Name)
-					PVC_client.Delete(context.TODO(), v.Name, v1.DeleteOptions{})
-				}
-			}
+		err = common.UninstallHelmRelease(clientset, helmClient, releaseName, namespace, deletePVCs)
+		if err != nil {
+			log.Fatalf("Error removing a release: %s", err)
 		}
 
 	},
